52b7f9df8436d1b9322fabc6d9c4a0d97df44e31
Joel Ccasane <joelccasane@gmail.com>
2024-10-12 07:46:54 -0500

    complete header and footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,66 @@
 <template>
-    <footer class="footer"></footer>
+    <footer class="footer">
+        <div class="footer__container">
+            <p>
+                © {{ dateFooter }} <a href="javascript:void(0)">TaskPro</a>.
+                Todos los derechos reservados.
+            </p>
+            <div class="footer__links">
+                <a href="javascript:void(0)" aria-label="Terms and Conditions"
+                    >Términos y Condiciones</a
+                >
+                <a href="javascript:void(0)" aria-label="Privacy Policy"
+                    >Política de Privacidad</a
+                >
+            </div>
+        </div>
+    </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const dateFooter = ref(new Date().getFullYear().toString());
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.footer {
+    background-color: $primary-color;
+    padding: 1.8rem 1.5rem;
+    &__container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        max-width: 980px;
+        margin: 0 auto;
+        width: 100%;
+        p {
+            color: $neutral-gray;
+            font-size: 0.875rem;
+        }
+        a {
+            text-decoration: none;
+            color: $neutral-gray;
+            transition: color 0.3s ease;
+
+            &:hover {
+                color: $background-light;
+            }
+        }
+    }
+    &__links {
+        display: flex;
+        gap: 1.3rem;
+
+        a {
+            font-size: 0.875rem;
+            text-decoration: none;
+            color: $neutral-gray;
+            transition: color 0.3s ease;
+
+            &:hover {
+                color: $background-light;
+            }
+        }
+    }
+}
+</style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,7 +1,28 @@
 <template>
-    <header class="header"></header>
+    <header class="header">
+        <div class="header__container">
+            <a href="/">TaskPro</a>
+        </div>
+    </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.header {
+    background-color: $primary-color;
+    &__container {
+        max-width: 980px;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        padding: 0.4rem 1.5rem;
+    }
+    a {
+        font-size: 2rem;
+        font-weight: 700;
+        color: $background-light;
+        text-decoration: none;
+    }
+}
+</style>
