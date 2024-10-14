30ce371be03728508719454be4973058f3d8a7df
Joel Ccasane <joelccasane@gmail.com>
2024-10-14 15:45:06 -0500

    update testimonials section and modify data.json

---
+++ src/components/CardTestimonial.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <div class="card__container">
+            <p>{{ description }}</p>
+        </div>
+        <div class="card__footer">
+            <p class="card__person">{{ person }}</p>
+            <span class="card__field">{{ field }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    description: string;
+    person: string;
+    field: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.card {
+    max-width: 310px;
+    color: $primary-color;
+    background-color: $background-light;
+    overflow: hidden;
+    transition:
+        transform 0.3s ease,
+        box-shadow 0.3s ease;
+
+    &:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 6px 8px rgba($primary-color, 0.5);
+    }
+
+    &__container {
+        padding: 1rem;
+    }
+
+    &__footer {
+        padding: 0.6rem 1rem;
+        background-color: $secondary-color;
+        color: $background-light;
+        p {
+            font-weight: bold;
+        }
+
+        span {
+            font-size: 0.875rem;
+        }
+    }
+}
+</style>
--- src/sections/TestimonialSection.vue
+++ src/sections/TestimonialSection.vue
@@ -1,7 +1,43 @@
 <template>
-    <section class="testimonials"></section>
+    <section class="testimonials">
+        <h2>TESTIMONIOS</h2>
+        <div class="testimonials__container">
+            <CardTestimonial
+                v-for="tes in testimonials"
+                :key="tes.description"
+                :description="tes.description"
+                :field="tes.field"
+                :person="tes.person"
+            ></CardTestimonial>
+        </div>
+    </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardTestimonial from '@/components/CardTestimonial.vue';
+import { testimonials } from '@/data/data.json';
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.testimonials {
+    padding: 4rem 1.5rem;
+    background-color: $neutral-gray;
+    h2 {
+        color: $primary-color;
+        text-decoration: underline;
+        text-underline-offset: 6px;
+        text-align: center;
+        margin-bottom: 2.5rem;
+        font-size: 2.5rem;
+    }
+    &__container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        gap: 1.5rem;
+        max-width: 980px;
+        width: 100%;
+        margin: auto;
+    }
+}
+</style>
