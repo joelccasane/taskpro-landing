4d0638c8d269b2fa732e08f387825f8715d2df8c
Joel Ccasane <joelccasane@gmail.com>
2024-10-15 09:32:18 -0500

    update store section and modify data.json

--- src/sections/StoreSection.vue
+++ src/sections/StoreSection.vue
@@ -1,7 +1,46 @@
 <template>
-    <section class="store"></section>
+    <section class="store">
+        <div class="store__container">
+            <h2>{{ store.title }}</h2>
+            <p>{{ store.description[0] }}</p>
+            <p>{{ store.description[1] }}</p>
+            <HeroButtons></HeroButtons>
+        </div>
+    </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { store } from '@/data/data.json';
+import HeroButtons from '@/components/HeroButtons.vue';
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.store {
+    background-color: $background-light;
+    padding: 5rem 1.5rem;
+    display: flex;
+    justify-content: center;
+
+    &__container {
+        width: 100%;
+        max-width: 980px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h2 {
+        max-width: 700px;
+        font-size: 3rem;
+        line-height: 1.1;
+        font-weight: 800;
+        text-align: center;
+    }
+    p {
+        max-width: 600px;
+        margin: 12px 0;
+        font-size: 20px;
+        color: $secondary-color;
+        text-align: center;
+    }
+}
+</style>
